d222747ff73d49a0c913c833edc5fefebcd7cb10
cyberchekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-06-07 20:34:02 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -23,7 +23,7 @@ if (Config.WORKTYPE == 'private') {
     Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```*ʜᴇʏʏ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ*\n*ᴛʏᴘᴇ .help ғᴏʀ ᴍᴇɴᴜ*\n*ᴏᴡɴᴇʀ wa.me/919895828468*\n*it isn't where you from. its where you're going that counts.*```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL , MessageType.text);
+            await message.client.sendMessage(message.jid,'*ʜᴇʏʏ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ*\n*ᴛʏᴘᴇ .help ғᴏʀ ᴍᴇɴᴜ*\n*ᴏᴡɴᴇʀ wa.me/919895828468*\n*it isn't where you from. its where you're going that counts.*\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL , MessageType.text);
         }N
         else {
             const pow = '*Made by AMAL_SER*'
@@ -83,7 +83,7 @@ else if (Config.WORKTYPE == 'public') {
     Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC, dontAddCommandList: true}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL, MessageType.text);
+            await message.client.sendMessage(message.jid,'*ʜᴇʏʏ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ*\n*ᴛʏᴘᴇ .help ғᴏʀ ᴍᴇɴᴜ*\n*ᴏᴡɴᴇʀ wa.me/919895828468*\n*it isn't where you from. its where you're going that counts.*\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Plugin Channel:* ' + Config.CHANNEL, MessageType.text);
         }
         else {
             const pow = '*Powered by WhatsAsena*'
