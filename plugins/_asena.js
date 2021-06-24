9ce1399176ec552c5c1ad5a0c9ec8221600ea72c
cyberchekuthan <80899895+cyberchekuthan@users.noreply.github.com>
2021-06-25 02:20:58 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -37,7 +37,7 @@ if (Config.WORKTYPE == 'private') {
                     }
                     CMD_HELP += '*🍒 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
                     if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
-                    if (command.usage !== '') CMD_HELP += '*🌿 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    if (command.usage !== '') CMD_HELP += '*🍭 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
                     if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
 
                 }
