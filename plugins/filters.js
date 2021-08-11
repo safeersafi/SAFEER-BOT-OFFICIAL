dfb2b4ec99018a69844a3c1a44c1ba86e2ce12a4
safeersafi <53081109+safeersafi@users.noreply.github.com>
2021-08-11 14:57:14 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -8,10 +8,10 @@ const Asena = require('../events');
 const {MessageType, Mimetype } = require('@adiwajshing/baileys'); 
 const FilterDb = require('./sql/filters');
 const Config = require('../config')
-const jid = Config.DISBGM != false ? Config.DISBGM.split(',') : [];
+
 const Language = require('../language'); 
 const Lang = Language.getString('filters'); 
-if (Config.WORKTYPE == 'private') { 
+
 Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {      match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);      if (match === null) {          filtreler = await FilterDb.getFilter(message.jid);          if (filtreler === false) {              await message.client.sendMessage(message.jid,Lang.NO_FILTER,MessageType.text)          } else {              var mesaj = Lang.FILTERS + '\n';              filtreler.map((filter) => mesaj += '```' + filter.dataValues.pattern + '```\n');              await message.client.sendMessage(message.jid,mesaj,MessageType.text);          }      } else {          if (match.length < 2) {              return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + ' ```.filter "sa" "as"',MessageType.text);          }          await FilterDb.setFilter(message.jid, match[0].replace(/['"“]+/g, ''), match[1].replace(/['"“]+/g, '').replace(/[#]+/g, '\n'), match[0][0] === "'" ? true : false);          await message.client.sendMessage(message.jid,Lang.FILTERED.format(match[0].replace(/['"]+/g, '')),MessageType.text);      } 
 })); 
 Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, dontAddCommandList: true}, (async (message, match) => {      match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);      if (match === null) {          return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)      }      del = await FilterDb.deleteFilter(message.jid, match[0].replace(/['"“]+/g, ''));            if (!del) {          await message.client.sendMessage(message.jid,Lang.ALREADY_NO_FILTER, MessageType.text)      } else {          await message.client.sendMessage(message.jid,Lang.DELETED, MessageType.text)      } 
